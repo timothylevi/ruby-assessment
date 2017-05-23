8b29317b81cfa419ac22dea2f395dda551e9638f
Tim Campbell <timothylevicampbell@gmail.com>
2017-05-23 17:33:11 -0400

    Init

---
+++ linkedList1.rb
@@ -0,0 +1,23 @@
+# Reversing a Linked List
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+def print_values(list_node)
+  print "#{list_node.value} --> "
+  if list_node.next_node.nil?
+    print "nil\n"
+    return
+  else
+    print_values(list_node.next_node)
+  end
+end
